738843da4a64ba8e7681658018eb895d0db866cf
arunk-web <arunk02615@gmail.com>
2026-09-19 23:18:50 +0530

    embedding generation pipeline

--- app/embedding.py
+++ app/embedding.py
@@ -1,11 +1,14 @@
 from sentence_transformers import SentenceTransformer
+from app.config import EMBEDDING_MODEL_NAME
 
+model = SentenceTransformer(EMBEDDING_MODEL_NAME)
 
-model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+def generate_embedding(text):
+    embedding = model.encode(text)
+    return embedding
 
 
 def generate_embedding_for_chunks(chunks):
     for chunk in chunks:
         chunk["embedding"] = model.encode(chunk["code"])
     return chunks
-    
